18edb0cbe139f5853e03f0af4e765f6d02181d03
hadifayazi <hadifayazi4@gmail.com>
2024-01-17 05:44:16 +0100

    creating UserRegistrationView

--- backend/realmback/accounts/views.py
+++ backend/realmback/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            if User.objects.filter(email=email).exists():
+                return Response({'error': 'User with this email already exists!'}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
