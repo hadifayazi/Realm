e735213c3ffdd51946e457ce9cc025b654ab99a7
hadifayazi <hadifayazi4@gmail.com>
2024-01-17 04:24:21 +0100

    creating cutom user model and user manager

--- backend/realmback/accounts/models.py
+++ backend/realmback/accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class UserAccountManager(BaseUserManager):
+    """
+    Manager to handle creating of users
+    """
+
+    def create_user(self, name, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required!')
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, name, email, password, **extra_fields):
+        user = self.create_user(name=name, email=email, password=password)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        user.save(using=self._db)
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User model based on django User
+    """
+
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self) -> str:
+        return self.email
